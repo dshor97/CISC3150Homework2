caace6a22e625e259ba63bc1200cb7b4853df983
Dayton Shor <dayton.shor@gmain.com>
2017-09-05 16:34:46 -0400

    Question 1 update

--- src/Question1.java
+++ src/Question1.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Question1 {
     public static void main(String[] args){
+        int numPyramid;
+        String layer1 = "";
+        String layer2 = "";
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many layers do you want: ");
+        numPyramid = input.nextInt();
+
+        for(int i = 1; i <= numPyramid;i++){
+            
+        }
 
     }
 }
