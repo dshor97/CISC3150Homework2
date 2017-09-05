f177d60334f31c11d241808641efd8dac690ae16
Dayton Shor <dayton.shor@gmain.com>
2017-09-05 17:45:40 -0400

    Question 1 update

--- src/Question1.java
+++ src/Question1.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args){
         int numPyramid;
-        String layer1 = "";
-        String layer2 = "";
-
         Scanner input = new Scanner(System.in);
         System.out.print("How many layers do you want: ");
         numPyramid = input.nextInt();
 
-        for(int i = 1; i <= numPyramid;i++){
-
+        for(int i = 0; i <= numPyramid; i++){
+            
         }
 
     }
