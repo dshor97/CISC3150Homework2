6403b9792c40ea5bc2eebd8bb39e0995ea70073d
Dayton Shor <dayton.shor@gmain.com>
2017-09-06 17:01:53 -0400

    Update Question1

--- src/Question2.java
+++ src/Question2.java
@@ -18,54 +18,67 @@ public class Question2 {
             isLeapYear = true;
         }
 
+        printCalender(month,year,firstDay,isLeapYear);
+
 
 
 
     }
-    public static void printCalender(int month, String day){
+    public static void printCalender(int month, int year, String day, boolean leapYear){
         switch (month){
             default:
                 System.out.println("Month is an invalid");
-                break;
+                return;
             case 1:
-                System.out.printf("","January " + 2015);
-
+                System.out.println("January " + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
             case 2:
-                System.out.printf("","January " + 2015);
+                System.out.println("February " + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
             case 3:
-                System.out.printf("","January " + 2015);
+                System.out.println("March " + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
             case 4:
-                System.out.printf("","January " + 2015);
+                System.out.println("April " + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
             case 5:
-                System.out.printf("","January " + 2015);
+                System.out.println("May " + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
             case 6:
-                System.out.printf("","January " + 2015);
+                System.out.println("June " + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
             case 7:
-                System.out.printf("","January " + 2015);
+                System.out.println("July " + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
             case 8:
-                System.out.printf("","January " + 2015);
+                System.out.println("August " + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
             case 9:
-                System.out.printf("","January " + 2015);
+                System.out.printf("\n","September" + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
             case 10:
-
+                System.out.printf("\n","October" + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
             case 11:
-
+                System.out.printf("\n","November" + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
             case 12:
-
+                System.out.printf("\n","December" + year);
+                System.out.println("Mon Tue Wed Thu Fri Sat Sun");
                 break;
         }
-        printCalender((month + 1), day);
+        printCalender((month + 1), year, day, leapYear);
 
     }
 }
