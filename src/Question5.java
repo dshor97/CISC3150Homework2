072df025adceb18df2ce71a30c0675aefeedbb52
Dayton Shor <dayton.shor@gmain.com>
2017-09-06 15:06:45 -0400

    Finish Question5

--- src/Question5.java
+++ src/Question5.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Question5 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("put in some numbers separated by commas: ");
+
+        String test = input.nextLine();
+
+        input = new Scanner(test).useDelimiter(",");
+
+        while(input.hasNext()){
+            System.out.println(input.next());
+        }
+
+    }
 }
