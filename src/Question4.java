acdfed5688aa13bd373f1647d170f9850dfe25af
Dayton Shor <dayton.shor@gmain.com>
2017-09-06 16:09:31 -0400

    Finish Question5 and Question6

--- src/Question4.java
+++ src/Question4.java
@@ -1,5 +1,17 @@
 public class Question4 {
     public static void main(String[] args){
 
+        outerFor:
+        for(int i = 1; i <= 100; i++){
+            for(int j = 0; j <= 300; j++){
+                if((i + j) % 5 == 2) {
+                    System.out.println("The number is " + (i + j) + ". Breaking out of the inner loop.");
+                    break;
+                }else if((i + j) % 10 == 0){
+                    System.out.println("The number is " + (i + j) + ". Breaking out of outer loop");
+                    break outerFor;
+                }
+            }
+        }
     }
 }
