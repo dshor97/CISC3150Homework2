3f10653eb0d3bb97f8171f6f19f029b678f474eb
Dayton Shor <dayton.shor@gmain.com>
2017-09-06 18:31:39 -0400

    Finish Question1

--- src/Question1.java
+++ src/Question1.java
@@ -11,12 +11,13 @@ public class Question1 {
 
         for(int i = 1; i <= numPyramid; i++){
             for(int k = 1; k<= i; k++){
-                 layer = layer + " " + k;
+                 layer = layer + k;
             }
             for (int j = i-1; j >= 1; j--){
-                layer = layer + " " + j;
+                layer = layer + j;
             }
-            System.out.println(layer);
+            //System.out.println(layer.length());
+            System.out.printf("%"+ ((numPyramid - i) + layer.length()) + "s\n",layer);
             layer = "";
         }
 
