b9d7d47a316fb030ad2016edbffcdbf85cb25969
Dayton Shor <dayton.shor@gmain.com>
2017-09-06 14:36:36 -0400

    update question2

--- src/Question2.java
+++ src/Question2.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        int month = 1;
 
         int year;
         String firstDay;
@@ -19,6 +20,52 @@ public class Question2 {
 
 
 
+
+    }
+    public static void printCalender(int month, String day){
+        switch (month){
+            default:
+                System.out.println("Month is an invalid");
+                break;
+            case 1:
+                System.out.printf("","January " + 2015);
+
+                break;
+            case 2:
+                System.out.printf("","January " + 2015);
+                break;
+            case 3:
+                System.out.printf("","January " + 2015);
+                break;
+            case 4:
+                System.out.printf("","January " + 2015);
+                break;
+            case 5:
+                System.out.printf("","January " + 2015);
+                break;
+            case 6:
+                System.out.printf("","January " + 2015);
+                break;
+            case 7:
+                System.out.printf("","January " + 2015);
+                break;
+            case 8:
+                System.out.printf("","January " + 2015);
+                break;
+            case 9:
+                System.out.printf("","January " + 2015);
+                break;
+            case 10:
+
+                break;
+            case 11:
+
+                break;
+            case 12:
+
+                break;
+        }
+        printCalender((month + 1), day);
 
     }
 }
