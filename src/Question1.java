f812767757a70e681ba7bb26802e173d0307ff0b
Dayton Shor <dayton.shor@gmain.com>
2017-09-07 15:30:06 -0400

    Update Question1

--- src/Question1.java
+++ src/Question1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args){
         int numPyramid;
-        String layer = new String(" ");
+        String layer = new String("");
 
         Scanner input = new Scanner(System.in);
         System.out.print("How many layers do you want: ");
@@ -11,16 +11,16 @@ public class Question1 {
 
         for(int i = 1; i <= numPyramid; i++){
             for(int k = 1; k<= i; k++){
-                 layer = layer + k + " ";
+                 layer = layer + " " + k;
+
             }
             for (int j = i-1; j >= 1; j--) {
-                layer = layer + j + " ";
+                layer = layer + " " + j;
+
             }
-
-            //System.out.println(layer.length());
-            System.out.printf("%"+ ((numPyramid - i) + layer.length()) + "s\n",layer);
-            layer = " ";
+            layer.replace(" ","");
+            System.out.printf("%"+ ((numPyramid - (2*i)) + layer.length()) + "s\n",layer);
+            layer = "";
         }
-
     }
 }
