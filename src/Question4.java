f01a819b4041dd2d6a477f5f088c86cbb225ca50
Dayton Shor <dayton.shor@gmain.com>
2017-09-08 15:08:01 -0400

    Update Question4

--- src/Question4.java
+++ src/Question4.java
@@ -3,7 +3,7 @@ public class Question4 {
 
         int x = 2098234;
         for(int i=0;i<35;i++){
-            System.out.printf("%d\t\t%32s\n",i,Integer.toBinaryString(x>>>(i*2)));
+            System.out.printf("%d\t\t%32s\n",i,Integer.toBinaryString(x>>>i));
         }
 
         System.out.println();
