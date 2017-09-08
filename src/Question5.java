b8a22c24950df2bcb1a2e4c0e6443b333de5ee7e
Dayton Shor <dayton.shor@gmain.com>
2017-09-08 14:42:03 -0400

    Add Question5_Output.txt

--- src/Question5.java
+++ src/Question5.java
@@ -4,9 +4,11 @@ public class Question5 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        System.out.print("put in some numbers separated by commas: ");
+        System.out.print("Enter some text separated by commas: ");
 
         String test = input.nextLine();
+
+        System.out.println();
 
         input = new Scanner(test).useDelimiter(",");
 
