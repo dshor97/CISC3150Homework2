bd2b1d6e73ca948f510b53c778db6a45537c7e6d
Dayton Shor <dayton.shor@gmain.com>
2017-09-08 14:44:10 -0400

    Add Question5_Output.txt

--- src/Question6.java
+++ src/Question6.java
@@ -4,11 +4,12 @@ public class Question6 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        System.out.print("What string to you want to tokenize: ");
+        System.out.print("Enter a string to tokenize: ");
         String test = input.nextLine();
 
         input = new Scanner(test).useDelimiter(""); // To tokenize every character use a empty string as a delimiter
 
+        System.out.println();
         while(input.hasNext()){
             System.out.println(input.next());
         }
